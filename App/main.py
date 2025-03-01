e816ef00669607017c5f1fe8d5112d8c7a846a7d
Rithxh <85475413+Rithxh@users.noreply.github.com>
2025-03-01 00:58:33 -0800

    Refactored to reflect proper REST standards

--- App/main.py
+++ App/main.py
@@ -1,48 +1,50 @@
-from flask import Flask, json, request
+from flask import Flask, json
 import requests
 from datetime import datetime,timezone
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config.from_file(os.path.join(app.root_path,'config.json'),load=json.load)
 
 def authenticate():
     current = datetime.now(timezone.utc)
-    if not app.config.get('TOKEN')  or (current > datetime.fromisoformat(app.config['EXPIRES_AT'])):
-        params = {'client_id':app.config.get('CLIENT_ID'),'client_secret':app.config.get('CLIENT_SECRET')}
+    if not os.getenv('TOKEN')  or (current > datetime.fromisoformat(os.getenv('EXPIRES_AT'))):
+        params = {'client_id':os.getenv('CLIENT_ID'),'client_secret':os.getenv('CLIENT_SECRET')}
         response = requests.post('https://api.artsy.net/api/tokens/xapp_token',data=params)
         data = response.json()
-        print(data)
-        with open(os.path.join(app.root_path,'config.json'),'w') as file:
-            json.dump({'TOKEN':data['token'],'EXPIRES_AT':data['expires_at'],'CLIENT_ID':app.config.get('CLIENT_ID'),'CLIENT_SECRET':app.config.get('CLIENT_SECRET')},file)
-        app.config.from_file(os.path.join(app.root_path,'config.json'),load=json.load)
+        os.environ['TOKEN'] = data['token']
+        os.environ['EXPIRES_AT'] =data['expires_at']
 
+        with open(os.path.join(app.root_path,'.env'),'w') as file:
+            file.write(f"CLIENT_ID={os.environ['CLIENT_ID']}\n")
+            file.write(f"CLIENT_SECRET={os.environ['CLIENT_SECRET']}\n")
+            file.write(f"TOKEN={data['token']}\n")
+            file.write(f"EXPIRES_AT={data['expires_at']}\n")
+        return data['token']
+    return os.environ['TOKEN']
+                    
 
 @app.route('/', methods=['GET','POST'])
 def renderPage():
-    authenticate()
     return app.send_static_file('templates/index.html')
 
-@app.route('/artists',methods=['GET','POST'])
-def get_artists():
-    print(app.config)
-    requestData = request.get_json()
-    name = requestData.get('query')
+@app.route('/artists/<string:artist>',methods=['GET'])
+def get_artists(artist):
     artists=[]
-    params = {'q':name, 'size':'10', 'type':'artist'}
-    headers = {'X-XAPP-Token':app.config.get('TOKEN')}
+    params = {'q':artist, 'size':'10', 'type':'artist'}
+    headers = {'X-XAPP-Token':authenticate()}
     response = requests.get('https://api.artsy.net/api/search',data=params,headers=headers)
-    data = response.json()['_embedded']['results']
+    data = response.json().get('_embedded').get('results')
     for obj in data:
         new_artist={'title':obj['title'],'id':obj['_links']['self']['href'].split('/')[5],'image':obj['_links']['thumbnail']['href']}
         artists.append(new_artist)
     return artists
 
-@app.route('/bio',methods=['GET','POST'])
-def get_artist_bio():
-    requestData = request.get_json()
-    id = requestData.get('id')
-    headers = {'X-XAPP-Token':app.config.get('TOKEN')}
+@app.route('/bio/<string:id>',methods=['GET'])
+def get_artist_bio(id):
+    headers = {'X-XAPP-Token':authenticate()}
     response = requests.get(f'https://api.artsy.net/api/artists/{id}',headers=headers)
     data = response.json()
     bios={'name':data['name'],'birthday':data['birthday'],'deathday':data['deathday'],'nationality':data['nationality'],'biography':data['biography']}
--- App/static/scripts/script.js
+++ App/static/scripts/script.js
@@ -1,79 +1,78 @@
-function resetDivs(){
-    const artist_cards = document.getElementById('search-results-cards');
-    artist_cards.innerHTML = "";
-    artist_cards.hidden = true;
-    const artist_bio_card = document.getElementById('artist-bio');
-    artist_bio_card.innerHTML = "";
-    artist_bio_card.hidden = true;
-    document.getElementById('no-results').hidden = true;
-}
-
 async function getArtistDetails(event){
     event.preventDefault();
-    resetDivs();
+
+    const artist_bio_card = document.getElementById('artist-bio');
+    artist_bio_card.innerHTML = "";
+    artist_bio_card.style.display = "none";
 
     const name = document.getElementById('query').value;
-    document.getElementById('loader').hidden = false;
+    document.getElementById('loader').style.display = "block";
 
     const artist_cards = document.getElementById('search-results-cards');
 
     try{
-        const response =  await fetch('/artists',{
-            method: 'POST',
-            headers: new Headers({'Content-Type':'application/JSON'}),
-            body: JSON.stringify({'query':name})
-        });
+        const response =  await fetch(`/artists/${name}`);
+        if(!response.ok){
+            throw new Error("Artist not found");
+        }
         const artists = await response.json();
 
-        artists.forEach(artist => {
-            const new_artist = document.createElement('div');
-            new_artist.classList.add('artist-card');
-            new_artist.id='artist-'+artist['id'];
+        artist_cards.innerHTML = "";
 
-            const artist_thumbnail = document.createElement('img');
-            artist_thumbnail.classList.add('artist-thumbnail');
-            artist_thumbnail.src = artist['image'].includes('missing_image')?'/static/images/artsy_logo.svg':artist['image'];
+        if(artists && artists.length){
+            artists.forEach(artist => {
+                const no_results_card = document.getElementById('no-results');
+                no_results_card.style.display = "none";
 
-            const artist_name = document.createElement('p');
-            artist_name.textContent = artist['title'];
-
-            new_artist.addEventListener('click',()=>getArtistBio(artist['id']));    
-
-            new_artist.appendChild(artist_thumbnail);
-            new_artist.appendChild(artist_name);
-            artist_cards.appendChild(new_artist);
-        })
+                const new_artist = document.createElement('div');
+                new_artist.classList.add('artist-card');
+                new_artist.id='artist-'+artist['id'];
+    
+                const artist_thumbnail = document.createElement('img');
+                artist_thumbnail.classList.add('artist-thumbnail');
+                artist_thumbnail.src = artist['image'].includes('missing_image')?'/static/images/artsy_logo.svg':artist['image'];
+    
+                const artist_name = document.createElement('p');
+                artist_name.textContent = artist['title'];
+    
+                new_artist.addEventListener('click',()=>getArtistBio(artist['id']));    
+    
+                new_artist.appendChild(artist_thumbnail);
+                new_artist.appendChild(artist_name);
+                artist_cards.appendChild(new_artist);
+            })
+        }else{
+            const no_results_card = document.getElementById('no-results');
+            no_results_card.style.display = "block";
+        }
     }catch(error){
         console.error("Error fetching artists ",error)
     }finally{
-        document.getElementById('loader').hidden = true;
-        if(document.getElementById('search-results-cards').childElementCount>0)
-            document.getElementById('search-results-cards').hidden = false;
-        else
-            document.getElementById('no-results').hidden = false;
+        document.getElementById('loader').style.display = "none";
     }  
 }
 
 async function getArtistBio(id){
-    document.querySelectorAll('.artist-card').forEach(el=>{el.style.backgroundColor = '#205375' });
-    document.getElementById('loader').hidden = false;
-    document.getElementById('artist-'+id).style.backgroundColor = '#112B3C';
+    document.querySelectorAll('.artist-card').forEach(el=>{el.classList.remove('selected-card')});
+    document.getElementById('loader').style.display = "block";
+    const selectedArtist = document.getElementById('artist-'+id);
+    selectedArtist.classList.add('selected-card');
 
     const artist_bio_card = document.getElementById('artist-bio');
+    artist_bio_card.style.display = "flex";
     artist_bio_card.innerHTML = "";
 
     try{
-        const response = await fetch('/bio',{
-            method: 'POST',
-            headers: new Headers({'Content-Type':'application/JSON'}),
-            body:JSON.stringify({'id':id})
-        });
+        const response = await fetch(`/bio/${id}`);
+        if(!response.ok){
+            throw new Error("Artist not found");
+        }
         const bio = await response.json();
         
-        const artist_details = document.createElement('h3');
+        const artist_details = document.createElement('h2');
         artist_details.textContent = bio['name'] + " (" + bio['birthday'] + "-" + bio['deathday'] + ")";
 
-        const artist_nation = document.createElement('h4');
+        const artist_nation = document.createElement('h3');
         artist_nation.textContent = bio['nationality'];
 
         const artist_bio = document.createElement('p');
@@ -85,7 +84,6 @@ async function getArtistBio(id){
     }catch(error){
         console.error("Error fetching bio ",error)
     }finally{
-        document.getElementById('loader').hidden = true;
-        document.getElementById('artist-bio').hidden = false;
+        document.getElementById('loader').style.display = "none";
     }
 }
